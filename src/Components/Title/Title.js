e32da1f5317740dc516a2f3fc24bf6465aed8ccd
ArianHamdi <59548833+ArianHamdi@users.noreply.github.com>
2021-03-29 05:25:00 -0700

    og5

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -20,7 +20,7 @@ const Title = ({ value }) => {
             <title>{`${uppercaseTitle} Music World`}</title>
             <meta property="og:title" content="Title" />
             <meta property="og:site_name" content="Music world" />
-            <meta property="og:description" content="open the world of music" />
+            <meta property="og:description" content="open the world of music open the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of musicopen the world of music" />
             <meta property="og:image" content="https://vistapointe.net/images/music-4.jpg" />
         </Helmet>
         // <div></div>
