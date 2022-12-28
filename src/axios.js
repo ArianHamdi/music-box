412114053ca4eaea9ba42bd17279de788b66039c
arian.hamdi <59548833+ArianHamdi@users.noreply.github.com>
2022-12-28 14:38:25 +0330

    .

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://music-box-inter.herokuapp.com/',
+    // baseURL: 'https://music-box-inter.herokuapp.com/',
     // baseURL: 'https://intermediary.herokuapp.com/',
+    baseURL: 'https://intermediary-production.up.railway.app/',
 })
 
 instance.interceptors.response.use(response => {
